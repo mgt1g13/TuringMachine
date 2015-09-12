32b837f7615b4da654286205bedd763bf2f560f8
Matheus Trindade <matheustrindade@Air-de-Matheus.home>
2015-09-12 17:36:01 -0300

    Added transition map generation

--- Parser.py
+++ Parser.py
@@ -1,30 +1,79 @@
-
-
+import re
 from sys import stdin
+from Transition import Transition
 
 class Parser:
 	
 
-	def __strToInt(s):
-		return int(s)
+	def __strToSingleTapeTransition(self, string):
+		return re.match( '\((.*),(.*)\)=\((.*),(.*),(.*)\)', string).groups()
+
+
+	def __strToMultitapeTransition(self, line):
+		tapeTransitions = line[:len(line)-1].split('|')
+		transitions = list(map(self.__strToSingleTapeTransition, tapeTransitions))
+		# [('q7', '1', 'q7', '1', 'E')]
+		
+		preState = ""
+		postState = ""
+		preConditions = []
+		postConditions = []
+		movements = []
+
+		for i in range(0, len(tapeTransitions)):
+			(preState, preCondition, postState, postCondition, movement) = transitions[0]
+			postConditions += [postCondition]
+			preConditions += [preCondition]
+			movements += [movement]
+
+		return Transition(preState,preConditions, postState,postConditions, movements)
+
+
 
 	def parse(self):
 		#Gets the number of tapes
-		line = stdin.readline()
-		self.nTapes = int(line)
+		self.nTapes = int(stdin.readline())
 
 		#gets the aux values
 		line = stdin.readline()
-		self.auxValues = list(map(int, line[:len(line)-2].split(' ')))
+		self.auxValues = list(map(int, line[:len(line)-1].split(' ')))
 		print(self.auxValues)
 		
+		#get the states
+		line = stdin.readline()
+		self.states =  line[:len(line)-1].split(' ')
+		print(self.states)
+
+		#get the input Alphabet
+		line = stdin.readline()
+		self.inputAlphabet =  line[:len(line)-1].split(' ')
+		print(self.inputAlphabet)
+
+		#Get the machine Alphabet
+		line = stdin.readline()
+		self.machineAlphabet =  line[:len(line)-1].split(' ')
+		print(self.machineAlphabet)
 
 
 
-# parser = Parser()
+		self.transitions = dict()
+		for i in range(self.auxValues[3]):
+			line = stdin.readline()	
+			transition = self.__strToMultitapeTransition(line)	
+			if(transition.preState in self.transitions):
+				self.transitions[transition.preState] += [transition]
+			else:
+				self.transitions[transition.preState] = [transition]
+		
+		
+		print(self.transitions)
 
-# parser.parse()
 
-# print(parser.nTapes)
+
+
+
+parser = Parser()
+parser.parse()
+print(parser.nTapes)
 
 
---
+++ Transition.py
@@ -0,0 +1,12 @@
+class Transition:
+
+	def __init__(self, preState,preConditions, postState,postConditions, movements):
+		self.preConditions = preConditions
+		self.postConditions = postConditions
+		self.movements = movements
+		self.preState = preState
+		self.postState = postState
+
+
+
+
